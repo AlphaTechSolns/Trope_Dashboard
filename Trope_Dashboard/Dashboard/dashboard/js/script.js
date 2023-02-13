dc0dd3b3d097d6c15bfeecc1474e8023895eb21f
MathumithaArumugam <100556248+MathumithaArumugam@users.noreply.github.com>
2023-02-13 20:17:14 +0530

    datatables insertion into dashboard

--- Trope_Dashboard/Dashboard/dashboard/js/script.js
+++ Trope_Dashboard/Dashboard/dashboard/js/script.js
@@ -118,3 +118,26 @@ imgProfile.addEventListener('click', function () {
 
 
 
+$(document).ready(function() {
+  
+ 
+  
+    var table = $('#example').DataTable({ 
+          select: false,
+          "columnDefs": [{
+              className: "Name", 
+              "targets":[0],
+              "visible": false,
+              "searchable":false
+          }]
+      });
+  
+    
+    $('#example tbody').on( 'click', 'tr', function () {
+     
+      alert(table.row( this ).data()[0]);
+  
+  } );
+  });
+
+  
