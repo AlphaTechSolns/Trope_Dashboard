5b89a21217482a2718786dc6888b847389f1213d
MathumithaArumugam <100556248+MathumithaArumugam@users.noreply.github.com>
2023-03-14 21:04:45 +0530

    logo and form changes

--- js/form-wizard.js
+++ js/form-wizard.js
@@ -34,8 +34,8 @@ $(function(){
         titleTemplate : '<div class="title">#title#</div>',
         labels: {
             previous : 'Back',
-            next : '<i class="zmdi zmdi-arrow-right"></i>',
-            finish : '<i class="zmdi zmdi-arrow-right"></i>',
+            next : '<i class="bx bx-right-arrow-alt"></i>',
+            finish : '<i class="bx bx-right-arrow-alt"></i>',
             current : ''
         },
         onStepChanging: function (event, currentIndex, newIndex) { 
